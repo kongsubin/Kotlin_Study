947539cf7a88c2a13621e77476b43ca0550a1113
kongsubin <qls0919@naver.com>
2022-09-20 09:25:11 +0900

    :dizzy: 커스텀뷰 기능 추가

--- TiltSensor/app/src/main/java/com/kongsub/tiltsensor/MainActivity.kt
+++ TiltSensor/app/src/main/java/com/kongsub/tiltsensor/MainActivity.kt
@@ -11,11 +11,16 @@ import android.os.Bundle
 import android.util.Log
 import android.view.WindowManager
 
+// 4-1. SensorEventListener : 센서 감지 클래스 상속
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
+    // 1. sensorManager lazy 초기화
     private val sensorManager by lazy {
         getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
+
+    // TiltView lazy 초기화
+    private lateinit var tiltView: TiltView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // 화면이 꺼지지 않게 하기
@@ -23,18 +28,21 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         // 화면이 가로 모드로 고정되게끔 하기.
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // TiltView 초기화
+        tiltView = TiltView(this)
+        setContentView(tiltView)
     }
 
-    // 센서 사용 등록 (주로 센서는 onResume()메소드에서 등록한다.)
+    // 2. 센서 사용 등록 (주로 센서는 onResume()메소드에서 등록한다.)
     override fun onResume() {
         super.onResume()
-        sensorManager.registerListener(this,
-            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-            SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, // 센서값을 받을 곳을 지정
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), // getDefaultSensor : 센서의 종류를 지정.
+            SensorManager.SENSOR_DELAY_NORMAL) // 센서 값을 얼마나 자주 받을지.
     }
 
-    // 센서값이 변경되면 호출됨.
+    // 4-2. 메소드 재정의 - 센서값이 변경되면 호출됨.
     override fun onSensorChanged(event: SensorEvent?) {
         // value[0] : x축 값 : 위로 기울이면 -10~0, 아래로 기울이면 0~10
         // value[1] : y축 값 : 위로 기울이면 -10~0, 오른쪽으로 기울이면 0~10
@@ -43,13 +51,16 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             Log.d("MainActivity", "onSensorChanged: x : " + "${event.values[0]}, y : ${event.values[1]}, z : ${event.values[2]}")
         }
 
+        if (event != null) {
+            tiltView.onSensorEvent(event)
+        }
     }
-    // 센서 정밀도가 변경되면 호출됨.
+    // 4-3. 메소드 재정의 - 센서 정밀도가 변경되면 호출됨.
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
         TODO("Not yet implemented")
     }
 
-    // 센서 사용 해제
+    // 3. 센서 사용 해제
     override fun onPause() {
         super.onPause()
         sensorManager.unregisterListener(this)
--- TiltSensor/app/src/main/java/com/kongsub/tiltsensor/TiltView.kt
+++ TiltSensor/app/src/main/java/com/kongsub/tiltsensor/TiltView.kt
@@ -1,5 +1,59 @@
 package com.kongsub.tiltsensor
 
-class TiltView {
-    
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.hardware.SensorEvent
+import android.view.View
+
+class TiltView(context: Context?) : View(context) {
+    private val bluePaint: Paint = Paint()
+    private val blackPaint: Paint = Paint()
+
+    init {
+        // 녹색 페인트
+        bluePaint.color = Color.BLUE
+
+        // 검은색 테두리 페인트
+        blackPaint.style = Paint.Style.STROKE
+    }
+
+    // 중점의 좌표 구하기
+    private var cX: Float = 0f
+    private var cY: Float = 0f
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        cX = w / 2f
+        cY = h / 2f
+    }
+    // 중점의 좌표 구하기
+
+    // 센서 값을 뷰에 반영
+    private var xCoord: Float = 0f
+    private var yCoord: Float = 0f
+
+    fun onSensorEvent(event: SensorEvent) {
+        // 화면이 가로이므로, x축과 y축 바꾸기.
+        yCoord = event.values[0] * 20
+        xCoord = event.values[1] * 20
+
+        // onDraw를 다시 부르는 메서드
+        invalidate()
+    }
+    // 센서 값을 뷰에 반영
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        // 원 그리기
+        canvas?.drawCircle(cX, cY, 100f, blackPaint)    // 바깥 원
+        canvas?.drawCircle(xCoord + cX, yCoord + cY, 100f, bluePaint)     // 안쪽 원
+
+        // 선 그리기 - 가운데 십자가
+        canvas?.drawLine(cX - 20f, cY, cX + 20f, cY, blackPaint)
+        canvas?.drawLine(cX, cY - 20f, cX, cY + 20f, blackPaint)
+    }
+
+
 }
