305ca3140993fe3a19c3318157ddfae931c9ec16
kongsubin <qls0919@naver.com>
2022-09-22 10:15:50 +0900

    :seedling: 프로젝트 초기 세팅

---
+++ MyGallery/app/src/main/java/com/kongsub/mygallery/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kongsub.mygallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
