736fb0b91a79a06c1f155668f073074137cef5a0
kongsubin <qls0919@naver.com>
2022-09-27 10:14:11 +0900

    :sparkles: Add View Binding

--- Flashlight/app/src/main/java/com/kongsub/flashlight/MainActivity.kt
+++ Flashlight/app/src/main/java/com/kongsub/flashlight/MainActivity.kt
@@ -2,10 +2,15 @@ package com.kongsub.flashlight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kongsub.flashlight.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
     }
 }
