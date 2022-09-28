9703363f4e0ac4303a417ea7f7db26bfbf4c7664
kongsubin <qls0919@naver.com>
2022-09-28 09:23:16 +0900

    :wrench: 가로모드로 고정

--- Xylophone/app/src/main/java/com/kongsub/xylophone/MainActivity.kt
+++ Xylophone/app/src/main/java/com/kongsub/xylophone/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kongsub.xylophone
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kongsub.xylophone.databinding.ActivityMainBinding
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // 화면이 가로모드로 고정되게 하기
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
     }
