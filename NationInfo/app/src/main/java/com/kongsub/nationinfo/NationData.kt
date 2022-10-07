4a2d20513751641e716152eda0c810075d4b0014
kongsubin <qls0919@naver.com>
2022-10-07 10:03:31 +0900

    :sparkles: 기능 추가

---
+++ NationInfo/app/src/main/java/com/kongsub/nationinfo/NationData.kt
@@ -0,0 +1,13 @@
+package com.kongsub.nationinfo
+
+/**
+ * Created by nanjui on 2016. 9. 11..
+ */
+data class NationDetailData (val name:String,
+                             val capital:String,
+                             val location:String,
+                             val volume:String,
+                             val weather:String,
+                             val language:String)
+
+data class GsonData(val data:ArrayList<NationDetailData>)
