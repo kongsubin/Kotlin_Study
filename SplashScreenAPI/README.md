a4481b40756132f11ac855823043ff41a4f77818
kongsubin <qls0919@naver.com>
2022-10-12 11:21:32 +0900

    :zap: splash 애니메이션 적용

--- SplashScreenAPI/README.md
+++ SplashScreenAPI/README.md
@@ -2,3 +2,4 @@
 https://proandroiddev.com/splash-screen-in-android-3bd9552b92a5
 
 https://developer.android.com/develop/ui/views/launch/splash-screen
+
--- SplashScreenAPI/app/src/main/java/com/kongsub/splashscreenapi/MainActivity.kt
+++ SplashScreenAPI/app/src/main/java/com/kongsub/splashscreenapi/MainActivity.kt
@@ -1,15 +1,76 @@
 package com.kongsub.splashscreenapi
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.animation.AnticipateInterpolator
+import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
+    var isReady = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Enable support for Splash Screen API for
         // proper Android 12+ support
         installSplashScreen()
         setContentView(R.layout.activity_main)
+
+        /* Customising the Splash Screen time */
+        thread(start=true) {
+            for (i in 1..5) {
+                Thread.sleep(500)
+            }
+            isReady = true
+        }
+
+        // Set up an OnPreDrawListener to the root view.
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    // Check if the initial data is ready.
+                    return if (isReady) {
+                        // The content is ready; start drawing.
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        // The content is not ready; suspend.
+                        false
+                    }
+                }
+            }
+        )
+        /* Customising the Splash Screen time */
+
+        /* Accessing the Splash Screen */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                // Create your custom animation.
+                val slideUp = ObjectAnimator.ofFloat(
+                    splashScreenView,
+                    View.TRANSLATION_Y,
+                    0f,
+                    -splashScreenView.height.toFloat()
+                )
+
+                slideUp.interpolator = AnticipateInterpolator()
+                slideUp.duration = 1000L
+
+                // Call SplashScreenView.remove at the end of your custom animation.
+                slideUp.doOnEnd { splashScreenView.remove() }
+
+                // Run your animation.
+                slideUp.start()
+            }
+        }
+        /* Accessing the Splash Screen */
+
     }
 }
