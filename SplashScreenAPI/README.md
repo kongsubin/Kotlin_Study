52b5f0b0890932930e16dec2d84b5061bda33a98
kongsubin <qls0919@naver.com>
2022-10-12 09:05:06 +0900

    :zap: splash 라이브러리 사용

---
+++ SplashScreenAPI/README.md
@@ -0,0 +1,4 @@
+# 참고
+https://proandroiddev.com/splash-screen-in-android-3bd9552b92a5
+
+https://developer.android.com/develop/ui/views/launch/splash-screen
---
+++ SplashScreenAPI/app/src/main/java/com/kongsub/splashscreenapi/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.kongsub.splashscreenapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Enable support for Splash Screen API for
+        // proper Android 12+ support
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+    }
+}
