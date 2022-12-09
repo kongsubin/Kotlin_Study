8e649dee277c2aa8f5d9d7522a46bc785293ef19
kongsubin <qls0919@naver.com>
2022-12-09 09:23:36 +0900

    :card_file_box: add data structure

--- Jetpack Compose/BusanCity/app/src/main/java/com/kongsub/busancity/model/Place.kt
+++ Jetpack Compose/BusanCity/app/src/main/java/com/kongsub/busancity/model/Place.kt
@@ -1,3 +1,13 @@
 package com.kongsub.busancity.model
 
-data class Place()
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Place(
+    val id: Int,
+    val startPoint: Double,
+    @StringRes val categoryId: Int,
+    @StringRes val titleResourceId: Int,
+    @DrawableRes val imageResourceId: Int,
+    @StringRes val newsDetails: Int
+)
